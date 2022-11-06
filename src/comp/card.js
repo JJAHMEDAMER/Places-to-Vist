118700fba44b3e581148acefa50c3e57f34051c7
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-06 16:39:42 +0200

    card template

--- src/comp/card.js
+++ src/comp/card.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Card = () => (
-  <div>
-    <h1>Card Hello World</h1>
+  <div className="card">
+    <img src="https://images.unsplash.com/photo-1519962551779-514fa155be9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80" className="card--img" alt="location img" />
+    <div className="card--text">
+      <div className="card--info">
+        <h1 className="card--title">Header</h1>
+        <p className="card--price">price</p>
+      </div>
+      <p className="card--desc">lorum </p>
+      <button className="card--button">Not Interisted</button>
+    </div>
   </div>
 );
 

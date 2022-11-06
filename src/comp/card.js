12709eb7b84a172c0b6b52ea5375de6306b3b26b
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-06 18:05:53 +0200

    hide and show text

--- src/comp/card.js
+++ src/comp/card.js
@@ -1,22 +1,33 @@
 import React from "react";
 
-const Card = (props) => (
-  <div className="card">
-    <img src={props.image} className="card--img" alt="location img" />
-    <div className="card--text">
-      <div className="card--info">
-        <h1 className="card--title">{props.name}</h1>
-        <p className="card--price">$ {props.price}</p>
+const Card = (props) => {
+  const [readMore, setReadMore] = React.useState(false);
+  function readMoreButton() {
+    setReadMore(!readMore);
+  }
+  return (
+    <div className="card">
+      <img src={props.image} className="card--img" alt="location img" />
+      <div className="card--text">
+        <div className="card--info">
+          <h1 className="card--title">{props.name}</h1>
+          <p className="card--price">$ {props.price}</p>
+        </div>
+        <p className="card--desc">
+          {readMore ? props.info : `${props.info.substring(0, 200)}...`}
+          <button className="card--read--more--button" onClick={readMoreButton}>
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button
+          className="card--button"
+          onClick={() => props.onClickHandler(props.id)}
+        >
+          Not Interested
+        </button>
       </div>
-      <p className="card--desc">{props.info}</p>
-      <button
-        className="card--button"
-        onClick={() => props.onClickHandler(props.id)}
-      >
-        Not Interested
-      </button>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
